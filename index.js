ed27a24f27894937542cf29e656336d1a667bbb3
rr <jhu>
2023-12-31 17:37:42 +0545

    learning inline function

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//pagination by default function
 const pagination = (page = 1, size = 5) => {
   for (let i = page; i <= size; i++) {
     console.log(i);
@@ -5,3 +6,13 @@ const pagination = (page = 1, size = 5) => {
 };
 
 pagination();
+
+// inline function--> assigning nameless/anonymous function to a variable which can be used repeatedly wherever we want
+//reusability.
+const footer = function () {
+  console.log("Anonymous");
+};
+
+footer();
+footer();
+footer();
